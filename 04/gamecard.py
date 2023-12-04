812442e971e3b41f003815a4c894faa94cc4620d
Macon Pegram <mpegram@captechconsulting.com>
2023-12-04 08:15:27 -0500

    More stage setting for solution with solution 1 still working.

--- 04/gamecard.py
+++ 04/gamecard.py
@@ -7,11 +7,25 @@ class GAMECARD:
         game_data = scrubbed.split("|")
         self._winners = set(map(int, filter(lambda val: len(val) > 0, (game_data[0].split(" ")))))
         self._plays = list(map(int, filter(lambda val: len(val) > 0, (game_data[1].split(" ")))))
+        self._count = 1 # how many copies do we have
 
-    def score(self):
-        total = 0
+    # Figures out how many winning matches we have
+    def wins(self):
+        count = 0
         for val in self._plays:
             if val in self._winners:
-                total = 1 if total == 0 else total << 1
+               count = count + 1
+        return count
 
-        return total
+    # scores the card using the solution 1 algorithm
+    def score(self):
+        wins = self.wins()
+        return 0 if wins == 0 else 1 << (wins - 1)
+
+    # Increase the number of copies of the card
+    def increment(self):
+        self._count = self._count + 1
+
+    # report the number of copies of the card
+    def how_many(self):
+        return self._count
