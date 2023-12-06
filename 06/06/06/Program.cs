fb97d85f96e920660a3f99b0bca6d13456bc5f29
Macon Pegram <mpegram@captechconsulting.com>
2023-12-06 08:37:44 -0500

    Simple, hacktastic, non-optimal approach due to limited time today.

---
+++ 06/06/06/Program.cs
@@ -0,0 +1,33 @@
+﻿// Very limited time today so just going for a quick and dirty solution. 
+// Even skipped parsing the data file.
+
+// Sample data        
+// process(new long[]{7, 15, 30}, new long[]{9, 40, 200}, 1);
+// process(new long[] {71530}, new long[] {940200}, 2);
+
+// Problem data
+process(new long[]{56, 97, 77, 93}, new long[]{499, 2210, 1097, 1440}, 1);
+process(new long[]{56977793}, new long[]{499221010971440}, 2);
+
+static void process(long[] times, long[] distances, long solutionNum)
+{
+    
+    long total = 1;
+    for (long idx = 0; idx < times.Length; idx++)
+    {
+        total *= findOptions(times[idx], distances[idx]);
+    }
+    Console.WriteLine($"Solution {solutionNum}: {total}");
+}
+
+static long findOptions(long time, long distance)
+{
+    long startVal = 2;
+    while (startVal * (time - startVal) <= distance)
+        startVal++;
+    
+    // last useful value will be the same distance from the end as start was from the beginning
+    long end = time - startVal;
+
+    return end - startVal + 1;
+}
